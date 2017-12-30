bff857617171743d48d0861abb0952850148e06f
Mona Temchin <monatemchin@gmail.com>
2017-12-30 18:05:24 -0500

    Updated simpleServerExpress.js

--- simpleServerExpress.js
+++ simpleServerExpress.js
@@ -1,14 +1,33 @@
 'use strict';
 
+var fs = require('fs');
 var path = require('path');
+var homePath = path.join(__dirname, 'home.html');
+var aboutPath = path.join(_dirname, 'about.html');
+var indexPath = path.join(_dirname, 'index.html');
 
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8000;
 
-app.get('/', function(req, res) {
-  var url = req.url;
-    });
+app.get('/simple/', function(req, res) {
+
+  const url = req.url;
+  const pars = url.parse(url);
+  var pathName = pars.pathName;
+  var fileName = pathname + '.html';
+
+
+     fs.readFile(pathName, 'utf8', function(err, fileName) {
+           if (err) {
+           console.error(err.stack);
+           return res.sendStatus(500);
+           }
+      res.send(fileName);   
+
+        });
+ });
+
 
 app.use(function(req, res) {
   res.sendStatus(404);
